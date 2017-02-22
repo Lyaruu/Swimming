5ddeed56c517746bed00f68640a1f2adcdd2646b
Lyaruu <lmlyaruu@hotmail.com>
2017-02-22 10:05:07 +0100

    Added Project

---
+++ Assets/Swimming.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Swimming : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+
+}
